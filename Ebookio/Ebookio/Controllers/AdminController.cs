acef605c38b1643bd9be50f553a380e4c4b924dd
jaypatel-007 <pixeliconic7@gmail.com>
2023-03-27 14:44:21 +0530

    adminlogin

---
+++ Ebookio/Ebookio/Controllers/AdminController.cs
@@ -0,0 +1,81 @@
+﻿using Ebookio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ebookio.Controllers
+{
+    public class AdminController : Controller
+    {
+        ebookioEntities eobj = new ebookioEntities();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AdminLogin()
+        {
+            HttpCookie cookie = Request.Cookies["AdminLoginDetails"];
+            if(cookie != null)
+            {
+                ViewBag.admin_emailid= cookie["admin_emailid"].ToString();
+                ViewBag.admin_password = cookie["admin_password"].ToString();
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AdminLogin(tbl_admin a)
+        {
+            HttpCookie cookie = new HttpCookie("AdminLoginDetails");
+            if(ModelState.IsValid==true)
+            {
+
+                if(a.RememberMe == true)
+                {
+                    cookie["admin_emailid"] = a.admin_emailid;
+                    cookie["admin_password"] = a.admin_password;
+                    cookie.Expires = DateTime.Now.AddDays(2);
+                    HttpContext.Response.Cookies.Add(cookie);
+                }
+                else
+                {
+                    cookie.Expires = DateTime.Now.AddDays(-1);
+                    HttpContext.Response.Cookies.Add(cookie);
+                }
+
+                var adminauth = eobj.tbl_admin.Where(model=>model.admin_emailid == a.admin_emailid && model.admin_password == a.admin_password).FirstOrDefault();
+                if(adminauth == null)
+                {
+                    ViewBag.msg = "Please Enter Correct EmailId Or Password";
+                    return View();
+                }
+                else
+                {
+                    Session["admin_username"] = adminauth.admin_username;
+                    return RedirectToAction("AdminDashboard");
+
+                }
+            }
+            return View();
+        }
+        public ActionResult AdminDashboard()
+        {
+            if(Session["admin_username"]==null)
+            {
+                return RedirectToAction("AdminLogin");
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+        public ActionResult AdminLogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("AdminLogin");
+        }
+    }
+}
---
+++ Ebookio/Ebookio/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ebookio")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ebookio")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8e47c967-5c3c-4719-bd5e-155833f0832c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
